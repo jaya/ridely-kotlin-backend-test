98181e5d12b76a893e3b09aa6d9d7f6f1974a890
osvaldsoza <osvald.soza@gmail.com>
2025-08-07 15:13:02 -0300

    Create drive service

--- src/main/kotlin/tech/jaya/ridely/service/DriverService.kt
+++ src/main/kotlin/tech/jaya/ridely/service/DriverService.kt
@@ -1,4 +1,18 @@
 package tech.jaya.ridely.service
 
-class DriverService {
+import org.springframework.stereotype.Service
+import tech.jaya.ridely.controller.DriverNotFound
+import tech.jaya.ridely.model.Driver
+import tech.jaya.ridely.repository.DriverRepo
+
+@Service
+class DriverService(
+    private val driverRepo: DriverRepo
+) {
+
+    fun findById(id: Long): Driver {
+        return driverRepo.findById(id).orElseThrow {
+            DriverNotFound("Drive not found $id")
+        }
+    }
 }
