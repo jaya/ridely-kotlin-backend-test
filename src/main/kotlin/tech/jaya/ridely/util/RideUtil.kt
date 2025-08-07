f78ed830446dfa2068ffc85853b99c849886b1ae
osvaldsoza <osvald.soza@gmail.com>
2025-08-07 20:32:07 -0300

    update RideUtil logic

--- src/main/kotlin/tech/jaya/ridely/util/RideUtil.kt
+++ src/main/kotlin/tech/jaya/ridely/util/RideUtil.kt
@@ -1,4 +1,14 @@
 package tech.jaya.ridely.util
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 class RideUtil {
+
+    fun calculaPrice(kmDistance: Double, timeMin: Double): BigDecimal {
+        val price = (kmDistance * 3.0 + timeMin * 2.0).toBigDecimal().setScale(2, RoundingMode.HALF_EVEN)
+        val taxaApp = price.multiply(BigDecimal("0.01")).setScale(2, RoundingMode.HALF_EVEN)
+
+        return price.subtract(taxaApp).setScale(2, RoundingMode.HALF_EVEN)
+    }
 }
