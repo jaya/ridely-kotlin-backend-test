dea76c48118a19008dd2c09191fec3bcec5963ec
osvaldsoza <osvald.soza@gmail.com>
2025-08-07 20:31:29 -0300

    update RideService logic and RideResponseDto structure

--- src/main/kotlin/tech/jaya/ridely/service/RideService.kt
+++ src/main/kotlin/tech/jaya/ridely/service/RideService.kt
@@ -1,4 +1,29 @@
 package tech.jaya.ridely.service
 
-class RideService {
+import org.springframework.stereotype.Service
+import tech.jaya.ridely.integration.googlemaps.GoogleMapsFeignClient
+import tech.jaya.ridely.service.dto.RideResponseDto
+import tech.jaya.ridely.util.RideUtil
+
+@Service
+class RideService(
+    private val googleMapsFeignClient: GoogleMapsFeignClient
+) {
+
+    fun requestRide(
+        origin: String, destination: String, key: String
+    ): RideResponseDto {
+        val googleMapsApiResponse = googleMapsFeignClient.getDirections(origin, destination, key);
+        val leg = googleMapsApiResponse.routeDtos.firstOrNull()?.legDtos?.firstOrNull() ?: throw RuntimeException("")
+        val kmDistance = (leg.distanceDto.value / 1000.0)
+        val estimedTimeMinutes = leg.durationDto.value / 60.0
+
+        val estimedPrice = RideUtil().calculaPrice(kmDistance, estimedTimeMinutes)
+
+        return RideResponseDto(
+            estimedTimeMinutes = String.format("%.2f", estimedTimeMinutes),
+            kmDistance = String.format("%.2f", kmDistance),
+            estimedPrice
+        )
+    }
 }
--- src/main/kotlin/tech/jaya/ridely/service/dto/RideResponseDto.kt
+++ src/main/kotlin/tech/jaya/ridely/service/dto/RideResponseDto.kt
@@ -1,8 +1,15 @@
-package tech.jaya.ridely.service
+package tech.jaya.ridely.service.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.math.BigDecimal
 
 data class RideResponseDto(
-    val tempoEstimadoMinutos: Double,
-    val distanciaKm: Double,
-    val precoEstimado: BigDecimal,
-    val taxaApp: BigDecimal
+    @field:JsonProperty("tempo_estimado_minutos")
+    val estimedTimeMinutes: String,
+
+    @field:JsonProperty("distancia_km")
+    val kmDistance: String,
+
+    @field:JsonProperty("preco_estimado")
+    val estimedPrice: BigDecimal
 )
