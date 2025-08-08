f515afce3bde5a56e2ddf3610c12179c8c6ea454
osvaldsoza <osvald.soza@gmail.com>
2025-08-07 21:39:10 -0300

    refactor(rise): adjust Service

--- src/main/kotlin/tech/jaya/ridely/service/RideService.kt
+++ src/main/kotlin/tech/jaya/ridely/service/RideService.kt
@@ -1,22 +1,74 @@
 package tech.jaya.ridely.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.RequestBody
+import tech.jaya.ridely.controller.*
 import tech.jaya.ridely.integration.googlemaps.GoogleMapsFeignClient
+import tech.jaya.ridely.repository.DriverRepo
+import tech.jaya.ridely.repository.RideRepo
 import tech.jaya.ridely.service.dto.RideResponseDto
 import tech.jaya.ridely.util.RideUtil
 
 @Service
 class RideService(
+    private val rideRepo: RideRepo,
+    private val driverRepo: DriverRepo,
     private val googleMapsFeignClient: GoogleMapsFeignClient
 ) {
 
+
+    @Value("\${google.maps.api.key}")
+    private val GOOGLE_MAPS_API_KEY: String = ""
+
+    fun requestDriver(req: RequestDriver): RequestDriverResponse {
+        val driver = driverRepo.findAvailableDriver().orElseThrow {
+            throw DriverUnavailable("We do not have drivers available")
+        }
+        val ride = req.toRide(driver)
+        ride.request(driver)
+        return RequestDriverResponse.fromRide(rideRepo.save(ride))
+    }
+
+    fun refuseRide(req: ActionRideRequest): RefuseResponse {
+        val id = req.id
+        val ride = rideRepo.findById(id).orElseThrow { RideNotFoundException("No ride found with id $id") }
+        ride.refuse()
+        return RefuseResponse.fromRide(rideRepo.save(ride))
+    }
+
+    fun deleteRide(req: ActionRideRequest): CancelResponse {
+        val id = req.id
+        val ride = rideRepo.findById(id).orElseThrow { RideNotFoundException("No ride found with id $id") }
+        ride.cancel()
+        return CancelResponse.fromRide(rideRepo.save(ride))
+    }
+
+    fun finishRide(req: FinishRideRequest): FinishResponse {
+        val (id, price) = req
+        val ride = rideRepo.findById(id).orElseThrow { RideNotFoundException("No ride found with id $id") }
+        ride.complete(price)
+        return FinishResponse.fromRide(rideRepo.save(ride))
+    }
+
+    fun acceptRide(req: ActionRideRequest): AcceptResponse {
+        val id = req.id
+        val ride = rideRepo.findById(id).orElseThrow { RideNotFoundException("No ride found with id $id") }
+        ride.accept()
+        return AcceptResponse.fromRide(rideRepo.save(ride))
+    }
+
+    fun delete(id: Long) {
+        return rideRepo.deleteById(id)
+    }
+
     fun requestRide(
-        origin: String, destination: String, key: String
+        origin: String, destination: String
     ): RideResponseDto {
-        val googleMapsApiResponse = googleMapsFeignClient.getDirections(origin, destination, key);
-        val leg = googleMapsApiResponse.routeDtos.firstOrNull()?.legDtos?.firstOrNull() ?: throw RuntimeException("")
-        val kmDistance = (leg.distanceDto.value / 1000.0)
-        val estimedTimeMinutes = leg.durationDto.value / 60.0
+        val googleMapsApiResponse = googleMapsFeignClient.getDirections(origin, destination, GOOGLE_MAPS_API_KEY)
+        val leg = googleMapsApiResponse.routes.firstOrNull()?.legs?.firstOrNull() ?: throw RuntimeException("")
+        val kmDistance = (leg.distance.value / 1000.0)
+        val estimedTimeMinutes = leg.duration.value / 60.0
 
         val estimedPrice = RideUtil().calculaPrice(kmDistance, estimedTimeMinutes)
 
